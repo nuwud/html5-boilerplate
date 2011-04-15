b957edcd659ff44c99611f1374a48b86f6fc87b8
Rob Larsen <rob@drunkenfist.com>
2011-04-15 00:23:35 -0400

    added skeleton singleton/namespace and "Markup-based unobtrusive comprehensive DOM-ready execution" INIT block

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
 /* Author: 
-
+Isobar
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ISOBAR = {
+	init: function(){
+		
+	}	
+}
+UTIL = {
+	fire : function(func,funcname, args){
+		var namespace = ISOBAR;  // indicate your obj literal namespace here
+		funcname = (funcname === undefined) ? 'init' : funcname;
+		if (func !== '' && namespace[func] && typeof namespace[func][funcname] == 'function'){
+			namespace[func][funcname](args);
+		} 
+	}, 
+	loadEvents : function(){
+		var bodyId = document.body.id;
+		// hit up common first.
+		UTIL.fire('common');
+		// do all the classes too.
+		$.each(document.body.className.split(/\s+/),function(i,classnm){
+			UTIL.fire(classnm);
+			UTIL.fire(classnm,bodyId);
+		});
+		UTIL.fire('common','finalize');
+	} 
+}; 
+// kick it all off here 
+$(document).ready(UTIL.loadEvents);
